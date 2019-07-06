016973c209727132ae9bd8ba353d10eb024b5de1
luogaofan <328272323@qq.com>
2019-07-06 19:55:03 +0800

    u

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
 import store from "./redux/store";
-import App from "./app";
+import App from "./App";
 
 ReactDOM.render(
   <Provider store={store}>
